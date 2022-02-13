b96d6709f431d93ac7ec4abb3fd3d7466b84b6dd
jcalvotorel <jcalvotorel@gmail.com>
2022-02-13 23:15:57 +0100

    feat: login form tests

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -13,7 +14,8 @@ describe('LoginComponent', () => {
       declarations: [ LoginComponent ],
       imports: [
         IonicModule.forRoot(),
-        TranslateModule.forRoot()
+        TranslateModule.forRoot(),
+        ReactiveFormsModule
       ]
     }).compileComponents();
 
@@ -25,4 +27,31 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get error control', () => {
+    expect(component.errorControl).toBeTruthy();
+  });
+
+  it('should submit form with valid values', () => {
+    const valid_form = {
+      'email': 'test@test.com',
+      'password': '12345', 
+      'reminder': 'true'
+    };
+    component.loginForm.setValue(valid_form);
+    component.submitForm();
+    expect(component.loginForm.valid).toEqual(true);
+  });
+
+  it('should submit form with invalid values', () => {
+    const invalid_form = {
+      'email': 'testtest.com',
+      'password': '1234',
+      'reminder': 'true'
+    };
+    component.loginForm.setValue(invalid_form);
+    component.submitForm();
+    expect(component.loginForm.valid).toEqual(false);
+  });
+
 });
